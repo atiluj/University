4a23a2ef14bdc965fbe7c8300f174f01a3969c20
atiluj <julita.osman@gmail.com>
2021-02-18 10:58:11 +0100

    .

--- SEM2/C/P3/zad1/compile.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-gcc -Wall -c decimalio.c -o decimalio.o
-gcc -Wall -c main.c -o main.o
-gcc -lm decimalio.o main.o -o program
--- SEM2/C/P5/zad1/compile.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash   // Julita Osman 314323 lista 4 zadanie 1
-gcc -Wall -c bst.c -o bst.o
-gcc -Wall -c main.c -o main.o
-gcc -lm bst.o main.o -o program
